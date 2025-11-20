05a07c63dd9d1fc3f0bacf676aaa64dba6aabb35
Istók Gellért <gellertistok@gmail.com>
2025-11-20 03:17:23 +0100

    ezért talán

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import { render } from 'preact';
 import { Main } from './panels/Main';		
 import './style.css';
 import "./Pwa"
-/**
- * Fő applikáció
- * @returns {JSXInternal.Element} A main eleme, ami összefogja a különböző pane-ket
- */
+
 export function App() {
   return <Main />;
 }
