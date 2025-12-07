0bf03bc25efd394b04c9be15c16475685fea4757
Istók Gellért <gellertistok@gmail.com>
2025-12-07 21:56:10 +0100

    cache fix

--- src/Pwa.ts
+++ src/Pwa.ts
@@ -6,7 +6,7 @@ class Pwa {
     constructor() {
         if (isSecureContext) {
             (async () => {
-                this.#serviceWorkerRegistration = await navigator.serviceWorker.register("sw.js", {updateViaCache: 'none'});
+                this.#serviceWorkerRegistration = await navigator.serviceWorker.register("sw.js");
             })();
         }
     }
