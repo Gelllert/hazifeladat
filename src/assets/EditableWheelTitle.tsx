8a3b96da04f673ff03e3b69125d179ac76515c42
Istók Gellért <gellertistok@gmail.com>
2026-06-28 20:20:10 +0200

    main fix

--- src/assets/EditableWheelTitle.tsx
+++ src/assets/EditableWheelTitle.tsx
@@ -28,6 +28,33 @@ export function EditableWheelTitle() {
         );
     }
 
+    if (title.toLowerCase().includes("gajdos")) {
+        const body = document.body;
+        const videoPath = 'warning_loop.mp4';
+        const videoHTML = `<video src="${videoPath}" autoplay playsinline
+                style="width: 100%; max-width: 800px; margin-bottom: 20px;"
+            ></video>`;
+        const errorHTML = `${videoHTML}
+        <h1>Ah hell naahh</h1><p>Majd jövőre, meg ilyenek kolléga <p>(<b>inkább ne</b>) Jöjjön legközelebb. Az alkalmazás működése felfüggesztve. <p>Terminating session, meg ilyenek<h1>szevasz</h1></p></p></p>`;
+        if (body) {
+            body.innerHTML = '';
+            body.style.margin = '0';
+            body.style.display = 'flex';
+            body.style.alignItems = 'center';
+            body.style.justifyContent = 'center';
+            body.style.minHeight = '100vh';
+            body.style.minWidth = '100vw';
+            body.style.backgroundColor = '#440000';
+            body.style.color = 'white';
+            body.style.flexDirection = 'column';
+            body.style.textAlign = 'center';
+            body.style.position = 'fixed';
+            body.style.top = '0';
+            body.style.left = '0';
+            body.innerHTML = errorHTML;
+        }
+    } 
+
     return (
         <h1 class="WheelTitleDisplay" onClick={() => setIsEditing(true)}>
             {title.toUpperCase()}
