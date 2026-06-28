f76f7052b200a90d0bd00e6b512dabdc354741d4
Istók Gellért <gellertistok@gmail.com>
2026-06-28 20:19:54 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = "webcache";
+const expectedCaches = [cacheName];
+
+async function impl(e) {
+    let cache = await caches.open(cacheName);
+    let cacheResponse = await cache.match(e.request);
+    if (cacheResponse)
+        return cacheResponse
+    else {
+        let networkResponse = await fetch(e.request);
+        cache.put(e.request, networkResponse.clone())
+        return networkResponse;
+    }
+}
+self.addEventListener("fetch", e => e.respondWith(impl(e))); 
+self.addEventListener("install", (e) => {
+    self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (!expectedCaches.includes(key)) {
+                        console.log('bocs de új cache van:', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); 
+});
+
